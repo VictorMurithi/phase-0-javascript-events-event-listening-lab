05e588620e31272bcfc3849b51d1c6cc1bbbf986
victor <victor.murithi@student.moringaschool.com>
2023-09-16 09:17:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const element = document.getElementById("button");
+if ("button") {
+    element.addEventListener("click", function() {
+        alert("Button clicked!");
+    });
+}
 }
